aeea72471f8d6cd07f2389ee33fee3aa1fa91390
ProjectUp <projectups@mail.ru>
2016-11-30 23:04:52 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-/**
- * Created by Edvard Asus on 11/29/2016.
- */
 'use strict'
-const express=require("express");
-const app=express();
-const Multer=require("multer");
-const upload=Multer({dest:"uploads/"});
+const express = require("express");
+const multer = require("multer");
+const path = require('path');
+const fs = require('fs');
 
-app.use(express.static("public"));
+const app = express();
 
-app.get("/",function(req,res){
-res.send("index");
+let storage = multer.diskStorage({
+    destination : function (req, file, cb) {
+        cb(null, './uploads');
+    },
+
+    filename : function (req, file, cb) {
+        let extStart=file.originalname.indexOf(path.extname(file.originalname));
+        cb(null, file.originalname.substr(0, extStart) + path.extname(file.originalname));
+    }
 });
 
+const upload = multer({storage : storage});
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/",function(req,res){
+    res.render("index", function (err, html) {
+        if (err) {
+            throw err;
+        }
+    });
+});
 
-app.post("/Pic",upload.any(),function(req,res,next){
-
-console.log("We received image Edo jan");
-res.send("image received");
-
-})
-
-
-
-
+app.post("/upload",upload.any(), function(req,res,next){
+   res.statusCode = 200;
+   res.send('image received');
+});
 
 
 app.listen(7777,console.log("server started"));
