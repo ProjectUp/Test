4fe8872a8fcf824da406ebce7698195f997304e6
ProjectUp <projectups@mail.ru>
2016-12-01 21:30:53 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require("express");
 const multer = require("multer");
 const path = require('path');
 const fs = require('fs');
+const bodyparser = require('body-parser');
 
 const app = express();
+app.use(bodyparser.urlencoded());
+app.use(bodyparser.json());
 
+let dest = ""
 let storage = multer.diskStorage({
     destination : function (req, file, cb) {
-        var dest="uploads/";
-        fs.mkdirSync(dest)
-        cb(null, dest);
+        cb(null, './public/users/' + dest);
     },
 
     filename : function (req, file, cb) {
@@ -19,20 +21,36 @@ let storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage : storage,dest:"uploads/"});
+const upload = multer({storage : storage});
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/",function(req,res){
+app.get("/",function(req, res){
     res.render("index", function (err, html) {
         if (err) {
+            res.statusCode = 404;
             throw err;
         }
     });
 });
+app.get("/public/form.html",function(req,res){
+    res.sendfile("./public/form.html");
+});
+
 
 app.post("/upload",upload.any(), function(req,res,next){
+    console.log(req.files);
    res.statusCode = 200;
    res.send('image received');
+});
+
+app.post("/text",function(req, res){
+    console.log(req.body);
+    dest=req.body.username;
+    fs.mkdirSync('./public/users/' + req.body.username);
+
+    res.statusCode = 200;
+    res.send("Done");
+
 });
 
 
