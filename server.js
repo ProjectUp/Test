2c76fe290fc3d6d20788d0893f0a374e20b7c651
ProjectUp <projectups@mail.ru>
2016-12-03 01:00:10 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -10,10 +10,50 @@ app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Functions
+const AddInfo=function(folder,Obj){
+fs.writeFile("./public/users/"+folder+"/name.txt",Obj.firstName,function(err){
+  if(err) console.log("There was an error while loading name");
+});//Writing Names
+fs.writeFile("./public/users/"+folder+"/Last_Name.txt",Obj.lastName,function(err){
+  if(err) console.log("There was an error while loading last name");
+});
+fs.writeFile("./public/users/"+folder+"/email.txt",Obj.email,function(err){
+  if(err) console.log("There was an error while loading email");
+});
+fs.mkdirSync('./public/users/' + folder+"/Private");
+fs.writeFile("./public/users/"+folder+"/Private/username.txt",Obj.username,function(err){
+  if(err) console.log("There was an error while loading username");
+});
+fs.writeFile("./public/users/"+folder+"/Private/password.txt",Obj.password,function(err){
+  if(err) console.log("There was an error while loading password");
+});
+if(Obj.team!=""){
+  fs.mkdirSync('./public/users/' + folder+"/Team");
+  fs.writeFile("./public/users/"+folder+"/Team/team.txt",Obj.team,function(err){
+    if(err) console.log("There was an error while loading Team Name");
+  });
+}
+return 1;
+
+}
+
+
+
+
+
+//End of Functions
+
+
+
+
+
+
 let dest = "";
 let storage = multer.diskStorage({
     destination : function (req, file, cb) {
-        cb(null, './public/users/' + dest);
+        fs.mkdirSync('./public/users/' + dest+"/Images");
+        cb(null, './public/users/' + dest+"/Images");
     },
 
     filename : function (req, file, cb) {
@@ -33,7 +73,7 @@ app.get("/",function(req, res){
         }
     });
 });
-app.get("/public/form.html",function(req, res){
+app.get("/public/form.html",function(req,res){
     res.sendfile("./public/form.html");
 });
 
@@ -41,7 +81,7 @@ app.get("/public/form.html",function(req, res){
 app.post("/upload",upload.any(), function(req,res,next) {
    console.log(req.files);
    res.statusCode = 200;
-   res.send('image received');
+   res.send('image received');//send another html file here
 });
 
 app.post("/text",function(req, res){
@@ -50,12 +90,13 @@ app.post("/text",function(req, res){
        if (exists) {
            console.log(exists);
            res.statusCode = 404;
-           res.send('user already exist');
+           res.send('user exists');
        } else {
-           fs.mkdirSync('./public/users/' + req.body.username);
            dest = req.body.username;
-           res.send("Done");
+           fs.mkdirSync('./public/users/' + req.body.username);
            res.statusCode = 200;
+           if(AddInfo(req.body.username,req.body))res.send("Done");
+
        }
     });
 });
