0ccd8c77832494a8f345a525ecd0fd8c401bc8dd
ProjectUp <projectups@mail.ru>
2016-12-04 20:33:09 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,84 +1,93 @@
 'use strict';
-const express=require("express");
-const fs=require("fs");
-const multer=require("multer");
-const app=express();
-const bodyparser=require("body-parser");
-const path=require("path");
+
+
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+const app = express();
+const bodyparser = require("body-parser");
+const path = require("path");
+
+
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 app.use(express.static("public"));
 
-const AddInfo=function(folder,Obj){
-fs.writeFile("./public/users/"+folder+"/name.txt",Obj.firstName,function(err){
-  if(err) console.log("There was an error while loading name");
-});//Writing Names
-fs.writeFile("./public/users/"+folder+"/Last_Name.txt",Obj.lastName,function(err){
-  if(err) console.log("There was an error while loading last name");
-});
-fs.writeFile("./public/users/"+folder+"/email.txt",Obj.email,function(err){
-  if(err) console.log("There was an error while loading email");
-});
-fs.mkdirSync('./public/users/' + folder+"/Private");
-fs.writeFile("./public/users/"+folder+"/Private/username.txt",Obj.username,function(err){
-  if(err) console.log("There was an error while loading username");
-});
-fs.writeFile("./public/users/"+folder+"/Private/password.txt",Obj.password,function(err){
-  if(err) console.log("There was an error while loading password");
-});
-if(Obj.team!=""){
-  fs.mkdirSync('./public/users/' + folder+"/Team");
-  fs.writeFile("./public/users/"+folder+"/Team/team.txt",Obj.team,function(err){
-    if(err) console.log("There was an error while loading Team Name");
-  });
-}
-return 1;
 
-}
+const AddInfo = function(folder, Obj) {
+    fs.writeFile("./public/users/" + folder + "/name.txt", Obj.firstName, function(err) {
+        if (err) console.log("There was an error while loading name");
+    }); //Writing Names
+    fs.writeFile("./public/users/" + folder + "/Last_Name.txt", Obj.lastName, function(err) {
+        if (err) console.log("There was an error while loading last name");
+    });
+    fs.writeFile("./public/users/" + folder + "/email.txt", Obj.email, function(err) {
+        if (err) console.log("There was an error while loading email");
+    });
+    fs.mkdirSync('./public/users/' + folder + "/Private");
+    fs.writeFile("./public/users/" + folder + "/Private/username.txt", Obj.username, function(err) {
+        if (err) console.log("There was an error while loading username");
+    });
+    fs.writeFile("./public/users/" + folder + "/Private/password.txt", Obj.password, function(err) {
+        if (err) console.log("There was an error while loading password");
+    });
+    if (Obj.team != "") {
+        fs.mkdirSync('./public/users/' + folder + "/Team");
+        fs.writeFile("./public/users/" + folder + "/Team/team.txt", Obj.team, function(err) {
+            if (err) console.log("There was an error while loading Team Name");
+        });
+    }
+    return 1;
 
+};
 
 
 
 
 let dest = "";
 let storage = multer.diskStorage({
-    destination : function (req, file, cb) {
-        fs.mkdirSync('./public/users/' + dest+"/Images");
-        cb(null, './public/users/' + dest+"/Images");
+    destination: function(req, file, cb) {
+        fs.mkdirSync('./public/users/' + dest + "/Images");
+        cb(null, './public/users/' + dest + "/Images");
     },
 
-    filename : function (req, file, cb) {
-        let extStart=file.originalname.indexOf(path.extname(file.originalname));
+    filename: function(req, file, cb) {
+        let extStart = file.originalname.indexOf(path.extname(file.originalname));
         cb(null, dest + path.extname(file.originalname));
     }
 });
 
-const upload = multer({storage : storage});
+const upload = multer({
+    storage: storage
+});
 
-app.get("/public/form.html",function(req,res,next){
-res.sendfile("./public/form.html");
+app.get("/public/form.html", function(req, res, next) {
+    res.statusCode = 200;
+    res.sendfile("./public/form.html");
 });
 
 
-app.post("/lol",upload.any(),function(req,res){
-console.log(req.files);
-res.send("OK");
+app.post("/upload", upload.any(), function(req, res) {
+    console.log(req.files);
+    res.statusCode = 200;
+    res.send("OK");
 });
-app.post("/TXT",function(req,res,next){
-console.log(req.body);
-fs.exists('./public/users/' + req.body.username, function (exists) {
-   if (exists) {
-       console.log(exists);
-       res.statusCode = 404;
-       res.send('user exists');
-   } else {
-       dest = req.body.username;
-       fs.mkdirSync('./public/users/' + req.body.username);
-       res.statusCode = 200;
-       if(AddInfo(req.body.username,req.body))res.send("Done");
+app.post("/TXT", function(req, res, next) {
+    console.log(req.body);
+    fs.exists('./public/users/' + req.body.username, function(exists) {
+        if (exists) {
+            console.log(exists);
+            res.statusCode = 404;
+            res.send('user exists');
+        } else {
+            dest = req.body.username;
+            fs.mkdirSync('./public/users/' + req.body.username);
+            res.statusCode = 200;
+            if (AddInfo(req.body.username, req.body)) res.send("Done");
 
-   }
+        }
+    });
+
 });
 
-})
-app.listen(3333,console.log("Server Started"));
+app.listen(3333, console.log("Server Started"));
